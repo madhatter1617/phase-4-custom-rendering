d86135e740d4ec438a57e3e604ee1d54c4582f9d
Rosa <rosarenee@att.net>
2021-08-30 20:12:57 -0500

    commit tree 1e28ebc8580a30e9be6778139f82bfdb26986b5b

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -2,14 +2,14 @@ class CheesesController < ApplicationController
 
   # GET /cheeses
   def index
-    cheeses = Cheese.all
-    render json: cheeses
+    eheeses = Cheese.all
+  render json: cheeses, only: [:id, :name, :price, :is_best_seller]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
   end
 
 end
